3287a6b47f6c32572a762ea1c75659d52bdc7a25
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2020-09-23 21:36:37 -0300

    added distance weights in knn imputation

--- ozonio/preprocessing.R
+++ ozonio/preprocessing.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(VIM)
 library(zoo)
 data <- read.csv("NY.csv")
-data <- data %>% select(Date.Local, O3.Mean) 
+data <- data %>% select(Date.Local, O3.Mean) %>% mutate(Date.Local = as.Date(Date.Local))
 #fulldata have NA rows in days that there is no observation
 fulldata <- data.frame(Date.Local = seq(from = min(data$Date.Local), to = max(data$Date.Local), by = 1))
 fulldata <- as.data.frame(merge(data, fulldata, all.y = TRUE)) %>% mutate(Date.Local = as.Date(Date.Local))
@@ -45,7 +45,7 @@ ggplot(data = as.data.frame(skips)) +
 #A Comparison of Various Imputation Methods for Missing
 #Values in Air Quality Data
 #kNN function can't recieve a date variable
-imputedData <- data.frame("obs" = index(fulldata), "O3.Mean" = dataNaN$O3.Mean)
+imputedData <- data.frame("obs" = index(fulldata), "O3.Mean" = fulldata$O3.Mean)
 imputedData <- kNN(imputedData, k = 7) #K = 7 because we'll consider the week of the missing data
 imputedData$Date.Local <- fulldata$Date.Local
 
@@ -69,19 +69,18 @@ ggplot() +
 #missing in sequence
 #I'm going to use different knn imputations for this year and for the rest of the data
 imputedData2 <- data.frame("obs" = index(fulldata), "O3.Mean" = imputedData$O3.Mean)
+
+
 imputedData2[((is.na(fulldata$O3.Mean)) 
               & (fulldata$Date.Local > "2011-01-01") 
               & (fulldata$Date.Local < "2012-01-01")),"O3.Mean"] <- NaN
-imputedData2 <- kNN(imputedData2, k = 120, )
-imputedData2$Date.Local <- fulldata$Date.Local
 
-ggplot() +
-  geom_point(data = imputedData2[is.na(fulldata$O3.Mean), ], aes(x = Date.Local, y = O3.Mean), colour = 'red') +
-  geom_point(data = fulldata, aes(x = Date.Local, y = O3.Mean), alpha = 0.2, fill = "black") +
-  scale_y_continuous(name = "O3") +
-  scale_x_date(date_breaks = "1 years", date_labels = "%Y", name = "Date") +
-  ggtitle("Real vs Imputed data (by separated imput.)") +
-  theme_bw()
+imputedData3 <- cbind(imputedData2)
+imputedData2 <- kNN(imputedData2, variable = c("O3.Mean"), k = 120, weightDist = FALSE)
+imputedData3 <- kNN(imputedData3, variable = c("O3.Mean"), numFun = weighted.mean, k = 120, weightDist = TRUE)
+imputedData2$Date.Local <- fulldata$Date.Local
+imputedData3$Date.Local <- fulldata$Date.Local
+
 
 ggplot() +
   geom_point(data = imputedData2[is.na(fulldata$O3.Mean), ], aes(x = Date.Local, y = O3.Mean), colour = 'red') +
@@ -90,3 +89,15 @@ ggplot() +
   scale_x_date(date_breaks = "1 years", date_labels = "%Y", name = "Date", limits = c(as.Date("2011-01-01"), as.Date("2012-01-01"))) +
   ggtitle("Real vs Imputed data (by separated imput.)") +
   theme_bw()
+
+ggplot() +
+  geom_point(data = imputedData3[is.na(fulldata$O3.Mean), ], aes(x = Date.Local, y = O3.Mean), colour = 'red') +
+  geom_point(data = fulldata, aes(x = Date.Local, y = O3.Mean), alpha = 0.2, fill = "black") +
+  scale_y_continuous(name = "O3") +
+  scale_x_date(date_breaks = "1 years", date_labels = "%Y", name = "Date", limits = c(as.Date("2011-01-01"), as.Date("2012-01-01"))) +
+  ggtitle("Real vs Imputed data (by separated imput., weighted by distance)") +
+  theme_bw()
+
+#Creating weekly data
+weekData <- cbind(fulldata)
+weekData$weekday <- format(fulldata$Date.Local, format = "%Y-%U")
